3a13ae6d7ea84d9e23640f11774710215cfc8d3a
Salvydas Lukosius <sall@w-ss.io>
2022-05-10 07:54:13 +0100

    modified:   .github/workflows/deploy.yml 	renamed:    .markdownlint.yaml -> .trunk/.markdownlint.yaml 	modified:   .trunk/trunk.yaml 	modified:   content/readme.md 	modified:   layouts/404.html 	modified:   layouts/partials/gitalk.html

--- lib/sh/push.sh
+++ lib/sh/push.sh
@@ -1,27 +1,49 @@
-#!/bin/env sh
+#!/usr/bin/env sh
+
+# shellcheck disable=SC2312
 
 if [ -n "$(git status -s)" ]; then
 	echo "The working directory is dirty. Please commit any pending changes."
 	exit 1
 fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+clean_tree() {
+  echo "Deleting old publication"
+  rm -rf public; mkdir public
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+  git worktree prune
+  rm -rf .git/worktrees/public/
 
-echo "Removing existing files"
-rm -rf public/*
+  echo "Checking out gh-pages branch into public"
+  git worktree add -B gh-pages public origin/gh-pages
 
-echo "Generating site"
-env HUGO_ENV="production" hugo -t github-style
+  echo "Removing existing files"
+  rm -rf public/*
+  return 0
+}
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+make_new() {
+  echo "Generating site"
+  env HUGO_ENV="production" hugo -t github-style --minify
+  return 0
+}
 
-echo "Pushing to github"
-git push --all
+publish_site() {
+  echo "Updating gh-pages branch"
+  cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
+
+  echo "Pushing to github"
+  git push --all
+  return 0
+}
+
+main() {
+  clean_tree
+  make_new
+  publish_site
+  exit 0
+}
+
+while true; do
+  MAIN "{@}"
+done
